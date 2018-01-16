869b8d9420b8f517d45b942bb300381add8d746f
Arturo <iglesias294@icloud.com>
2018-01-16 19:58:49 +0800

    firebase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Header } from './components/common';
+import firebase from 'firebase';
 
 class App extends Component {
+  componentWillMount(){
+    firebase.initializeApp({
+      apiKey: "AIzaSyCeq3H0kp_jO2OPFO93uwzYsTXiIXWLVYQ",
+      authDomain: "phone-auth-48825.firebaseapp.com",
+      databaseURL: "https://phone-auth-48825.firebaseio.com",
+      projectId: "phone-auth-48825",
+      storageBucket: "phone-auth-48825.appspot.com",
+      messagingSenderId: "73085473516"
+    });
+  
+  }
   render() {
     return (
       <View>
+        <Header headerText="Authentication" />
         <Text>An App</Text>
       </View>
     );
