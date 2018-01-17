0d8dbe002f597142b5d07a179387a3e31d9d758a
Arturo <iglesias294@icloud.com>
2018-01-17 16:01:42 +0800

    log in and logout functions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import firebase from 'firebase';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
   componentWillMount(){
     firebase.initializeApp({
       apiKey: "AIzaSyCeq3H0kp_jO2OPFO93uwzYsTXiIXWLVYQ",
@@ -13,13 +15,36 @@ class App extends Component {
       storageBucket: "phone-auth-48825.appspot.com",
       messagingSenderId: "73085473516"
     });
-  
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) { 
+      this.setState({ loggedIn: true });
+    } else {
+      this.setState({ loggedIn: false });
+    }
+  });
+}
+renderContent() {
+  switch(this.state.loggedIn) {
+    case true:
+    return (
+      <Button onPress={() => {firebase.auth().signOut()}}>
+        Log Out
+      </Button> 
+    )
+    case false: 
+      return <LoginForm />
+    default: 
+      return (
+        <View style={{flex:1, alignItems: 'center'}}>
+          <Spinner size="large" />
+        </View>)
   }
+}
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text>An App</Text>
+        {this.renderContent()}
       </View>
     );
   }
